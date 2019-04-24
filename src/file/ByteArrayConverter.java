38bd40e9110f91503876cceb97f3fa73e4f8bea2
HNTPC04 <HNTPC04@HNTPC04>
2019-04-24 09:34:26 +0900

    byte

---
+++ src/file/ByteArrayConverter.java
@@ -0,0 +1,29 @@
+package file;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ByteArrayConverter {
+    private byte[] convertByteArray(String filePath) {
+        FileInputStream fileInputStream = null;
+        byte[] bytesArray = null;
+        try {
+            File file = new File(filePath);
+            bytesArray = new byte[(int) file.length()];
+            fileInputStream = new FileInputStream(file);
+            fileInputStream.read(bytesArray);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return bytesArray;
+    }
+}
