b14845914bae55932dbedb16bae5a048d550e4b4
uijong0729 <krocha0729@gmail.com>
2021-10-30 23:11:31 +0900

    memo

---
+++ src/purejava/JavaCasting.java
@@ -0,0 +1,17 @@
+package purejava;
+
+public class JavaCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = (byte)128;
+		System.out.println(a); // -128
+		byte b = (byte)255;
+		System.out.println(b); // -1
+		byte c = (byte)767;
+		System.out.println(c); // -1
+		byte d = (byte)1023;
+		System.out.println(d); // -1
+	}
+
+}
