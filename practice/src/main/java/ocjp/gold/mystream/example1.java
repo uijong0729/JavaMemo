079165e174cb6ebab3e42b7b76cee1f8801eead0
uijong0729 <krocha0729@gmail.com>
2025-03-31 22:10:28 +0900

    map

--- practice/src/main/java/ocjp/gold/mystream/example1.java
+++ practice/src/main/java/ocjp/gold/mystream/example1.java
@@ -1,15 +1,44 @@
 package ocjp.gold.mystream;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
 public class example1 {
     public static void main(String[] args) {
         // java.util.NoSuchElementException
-        Optional<String> sample = Optional.ofNullable(null);
-        System.out.println(sample.get());
+        // Optional<String> sample = Optional.ofNullable(null);
+        // System.out.println(sample.get());
 
         // java.lang.NullPointerException
-        sample = Optional.of(null);
-        System.out.println(sample);
+        // sample = Optional.of(null);
+        // System.out.println(sample);
+
+        // ifPresentOrElse
+        Optional<String> sample3 = Optional.empty();
+        sample3.ifPresentOrElse(new Consumer<String>() {
+            public void accept(String t) {
+                System.out.println(t);
+            };
+        }, new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("empty");
+            }
+        });
+
+        // ifPresentOrElse lambda
+        sample3.ifPresentOrElse(str -> {
+            // consumer
+            System.out.println(str);
+        }
+        ,() -> {
+            // runnable
+            System.out.println("empty");
+        });
+
+        // map 사용법
+        Optional<Integer> a = Optional.of(100);
+        Optional<Integer> b = a.map(price -> price * 3);
+        System.out.println(b.get());
     }
 }
