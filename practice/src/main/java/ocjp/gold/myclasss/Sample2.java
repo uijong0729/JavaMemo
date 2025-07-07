030e8b5b394b15bac78c3bf39fd130662d2b39b7
uijong0729 <krocha0729@gmail.com>
2025-07-07 23:05:36 +0900

    74~80

---
+++ practice/src/main/java/ocjp/gold/myclasss/Sample2.java
@@ -0,0 +1,11 @@
+package ocjp.gold.myclasss;
+
+public class Sample2 {
+    static class Hello {
+
+    }
+    public static void main(String[] args) {
+        Sample2 b = new Sample2();
+        Sample2.Hello h = new Sample2.Hello();
+    }
+}
